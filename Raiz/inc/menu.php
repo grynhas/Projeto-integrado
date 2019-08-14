2628aecb9cd14a3594f301380dd7da7b2b78c83e
grynhas <patrick.d.s.reis@gmail.com>
2019-08-13 21:46:10 -0300

    ultimo commit do dia

--- Raiz/inc/menu.php
+++ Raiz/inc/menu.php
@@ -1,8 +1,8 @@
 <section  id="menu" class="col-2 col-md-2">
-  <div class="list-group" id="list-tab" role="tablist">
-    <a class="list-group-item list-group-item-action" href="perfil.php" id="list-profile-list" data-toggle="list" role="tab" aria-controls="profile">Perfil</a>
-    <a class="list-group-item list-group-item-action" href="mensagens.php" id="list-messages-list" data-toggle="list" role="tab" aria-controls="messages">Mensagens</a>
-    <a class="list-group-item list-group-item-action" href="favoritos.php" id="list-favorites-list" data-toggle="list" role="tab" aria-controls="settings">Favoritos</a>
-    <a class="list-group-item list-group-item-action" href="index.php" id="list-home-list" data-toggle="list" role="tab" aria-controls="home">Home</a>
+  <div class="list-group  menu" id="list-tab" role="tablist">
+    <a class="list-group-item list-group-item-action corItem" href="perfil.php" id="list-profile-list" data-toggle="list" role="tab" aria-controls="profile">Perfil</a>
+    <a class="list-group-item list-group-item-action corItem" href="mensagens.php" id="list-messages-list" data-toggle="list" role="tab" aria-controls="messages">Mensagens</a>
+    <a class="list-group-item list-group-item-action corItem" href="favoritos.php" id="list-favorites-list" data-toggle="list" role="tab" aria-controls="settings">Favoritos</a>
+    <a class="list-group-item list-group-item-action corItem" href="index.php" id="list-home-list" data-toggle="list" role="tab" aria-controls="home">Home</a>
   </div>
 </section>
