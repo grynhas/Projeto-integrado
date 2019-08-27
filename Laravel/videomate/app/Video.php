d26ba84c54e7aecf477f7b9c49f2953cc499480d
leonardo-openheimer <leonardo0openheimer@gmail.com>
2019-08-27 19:53:01 -0300

    Model Correction

--- Laravel/videomate/app/Video.php
+++ Laravel/videomate/app/Video.php
@@ -13,6 +13,6 @@ class Video extends Model
     'inicio_segundo', 'fim_hora', 'fim_munuto', 'fim_segundo'];
 
     public function user(){
-        return $this->hasOne(User:class, 'id', 'id_usuario');
+        return $this->hasOne(User::class, 'id', 'id_usuario');
     }
 }
