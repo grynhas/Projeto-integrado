46b49d06078c81f47b56ddfe49d71cfc3bff4861
leonardo-openheimer <leonardo0openheimer@gmail.com>
2019-08-27 20:29:49 -0300

    User Controller

---
+++ Laravel/videomate/app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function logandoUsuario(Request $request){
+        echo "em construção";
+        exit;
+    }
+
+    public function cadastrandoUsuario(Request $request){
+        echo "em construção";
+        exit;
+    }
+}
