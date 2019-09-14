3c73199e8e8cbb6c6ce0890611fbe598bb6ce107
grynhas <patrick.d.s.reis@gmail.com>
2019-09-13 22:19:35 -0300

    cadastro de usuarios funcional

--- Laravel/videomate/app/Http/Controllers/Auth/RegisterController.php
+++ Laravel/videomate/app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'nickname'=>['required','string' , 'max:255'],
         ]);
     }
 
@@ -67,6 +68,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'nickname'=>$data['nickname'],
         ]);
+
     }
 }
