02518837599a6d4bee21e1b619464ba5cf84c3f1
grynhas <patrick.d.s.reis@gmail.com>
2019-10-02 21:52:58 -0300

    adicionando comentarios no Reproduzuir e iconios no modal

--- Laravel/videomate/app/Http/Controllers/VideoController.php
+++ Laravel/videomate/app/Http/Controllers/VideoController.php
@@ -56,7 +56,42 @@ class VideoController extends Controller
         return redirect('/index');
         
     } 
+    public function alterandoFilme(Request $request, $id) { 
+        $video = Video::find($id);
+        
+        $request->validate([
+            'id_youtube' => 'required|string|max:1000',
+            'titulo' => 'required|string|max:50',
+            'descricao' => 'required|string|max:1000',
+            'tags' => 'required|string|max:1000',
+            'inicio_hora' => 'required|integer',
+            'inicio_minuto' => 'required|integer',
+            'inicio_segundo' => 'required|integer',
+            'fim_hora' => 'required|integer',
+            'fim_minuto' => 'required|integer',
+            'fim_segundo' => 'required|integer'
+        ]);
+        
+        $video = Video::create([
+            "id_usuario" => $request->input('id_usuario'),
+            "id_youtube" => $request->input('id_youtube'),
+            "titulo" => $request->input('titulo'),
+            "descricao" => $request->input('descricao'),
+            "tags" => $request->input('tags'),
+            "inicio_hora" => $request->input('inicio_hora'),
+            "inicio_minuto" => $request->input('inicio_minuto'),
+            "inicio_segundo" => $request->input('inicio_segundo'),
+            "fim_hora" => $request->input('fim_hora'),
+            "fim_minuto" => $request->input('fim_minuto'),
+            "fim_segundo" => $request->input('fim_segundo')
 
+        ]);
+
+        $video->save();
+
+        return redirect('/index');
+        
+    } 
     //Atulização 22.09
     public function listandoMeusVideos($id_usuario)
     {
